fecf046f65ab202ebf298bfbb57e4c2de9e525f8
reddintester <reddin.tester@gmail.com>
2021-02-01 10:22:18 +0500

    updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION='5.3'
-./apache-jmeter-${VERSION}/bin/jmeter -n -t test.jmx -l LoadReports\results.jtl -e -o LoadReports
+./apache-jmeter-${VERSION}/bin/jmeter -n -t CartTest.jmx -l LoadReports\results.jtl -e -o LoadReports
 sleep 180
